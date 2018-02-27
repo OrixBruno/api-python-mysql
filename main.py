e50be503ce064841751f377dfd033850c1c6132f
Bruno Silva Do Nascimento <bsnascimento@brq.com>
2018-02-27 09:55:36 -0300

    Ajustano porta deploy

--- main.py
+++ main.py
@@ -64,6 +64,6 @@ def apagar_musica(codigo):
 #     return "<h1>Hello World</hi>"
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True,port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True,port=8000)
