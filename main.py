87e629dbcc71f71002ba4d338ac2876207f06e3a
Bruno Silva Do Nascimento <bsnascimento@brq.com>
2018-02-27 08:43:08 -0300

    Alterando porta

--- main.py
+++ main.py
@@ -73,6 +73,6 @@ def index():
     return "<h1>Hello World</hi>"
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True,port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True,port=8000)
